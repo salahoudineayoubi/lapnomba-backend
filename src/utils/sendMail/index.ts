ed04dc78770965f466ca5d0780c2871ee5a13bbf
Eng Salah <salahoudineayoubi@gmail.com>
2026-03-02 11:07:14 +0100

    add material donate

--- src/utils/sendMail/index.ts
+++ src/utils/sendMail/index.ts
@@ -2,23 +2,23 @@
 import nodemailer from "nodemailer";
 
 export const sendMail = async (to: string, subject: string, text: string) => {
+
   const transporter = nodemailer.createTransport({
-    // @ts-ignore
-    host: process.env.SMTP_HOST, // Doit être node26-ca.n0c.com
-    port: Number(process.env.SMTP_PORT) || 465,
-    secure: true, // true pour le port 465
+    host: process.env.SMTP_HOST || "node26-ca.n0c.com", // ton serveur distant
+    port: Number(process.env.SMTP_PORT) || 465,          // port SSL
+    secure: true, // true pour 465, false pour 587
     auth: {
-      user: process.env.SMTP_USER,
-      pass: process.env.SMTP_PASS,
+      user: process.env.SMTP_USER || "contact@lapnomba.org",
+      pass: process.env.SMTP_PASS, // mot de passe du compte
     },
-   
     tls: {
-      rejectUnauthorized: false 
-    }
+      rejectUnauthorized: false, // permet de passer les certificats auto-signés si besoin
+    },
   });
 
+  // Envoyer l'email
   return await transporter.sendMail({
-    from: `"Fondation Lap Nomba" <${process.env.SMTP_USER}>`,
+    from: `"Fondation Lap Nomba" <${process.env.SMTP_USER || "contact@lapnomba.org"}>`,
     to,
     subject,
     text,
